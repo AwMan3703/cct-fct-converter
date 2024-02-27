44ed93f0cd1cc8c460ead98bd466261930a4bcbb
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-02-27 19:31:00 +0100

    imports

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
-import java.util.Scanner;
 import javax.imageio.ImageIO;
 
 
